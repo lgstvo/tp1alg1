63b155e1a36002e80a8ac8cc94aa24d4512bb138
lgstvo <lgustavo2001@gmail.com>
2021-01-20 11:32:17 -0300

    Adding base entry structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main(){
+
+    int numberOfCD, numberOfPV, temperatureDecay;
+
+    cin >> numberOfCD >> numberOfPV >> temperatureDecay;
+
+    vector <vector<int>> graphTree;
+    vector <int> connections;
+
+    for(int i = 0; i < numberOfCD; i++){
+        
+    }
+
+    for(int i = numberOfCD; i < numberOfPV+numberOfCD; i++){
+
+    }
+
 
     return 0;
 }
