7a073527f28c79aefee15bde3a83469a30faef3b
lgstvo <lgustavo2001@gmail.com>
2021-01-21 14:03:45 -0300

    CD, PV and TD getters and setters

--- src/headers/graph.hpp
+++ src/headers/graph.hpp
@@ -9,13 +9,24 @@
 class GraphTree{
     private:
         std::vector<std::vector<int>> adjencyList;
+        int CD;
+        int PV;
+        int temperatureDecay;
 
     public:
         GraphTree(std::vector<std::vector<int>>);
 
         std::vector<std::vector<int>> getAdjencyList();
+        int getCD();
+        int getPV();
+        int getTemperatureDecay();
+
+        void setCD(int);
+        void setPV(int);
+        void setTemperatureDecay(int);
         void setAdjencyList(std::vector<std::vector<int>>);
-        std::vector<int> dfs(int);
+        
+        std::vector<int> dfs();
 
 };
 
--- src/include/graph.cpp
+++ src/include/graph.cpp
@@ -8,11 +8,35 @@ std::vector<std::vector<int>> GraphTree::getAdjencyList(){
     return this->adjencyList;
 }
 
+int GraphTree::getCD(){
+    return this->CD;
+}
+
+int GraphTree::getPV(){
+    return this->PV;
+}
+
+int GraphTree::getTemperatureDecay(){
+    return this->temperatureDecay;
+}
+
+void GraphTree::setCD(int CD){
+    this->CD = CD;
+}
+
+void GraphTree::setPV(int PV){
+    this->PV = PV;
+}
+
+void GraphTree::setTemperatureDecay(int temperatureDecay){
+    this->temperatureDecay = temperatureDecay;
+}
+
 void GraphTree::setAdjencyList(std::vector<std::vector<int>> adjencyList){
     this->adjencyList = adjencyList;
 }
 
-std::vector<int> GraphTree::dfs(int decay){
+std::vector<int> GraphTree::dfs(){
     std::vector<int> reachedPV;
     return reachedPV;
 }
