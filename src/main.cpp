8ffa18c5651981cb54ed9b848bfe62e9ceac1bf9
lgstvo <lgustavo2001@gmail.com>
2021-01-21 13:39:12 -0300

    Main completion

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,32 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
 int main(){
 
     int numberOfCD, numberOfPV, temperatureDecay;
+    string entry;
 
     cin >> numberOfCD >> numberOfPV >> temperatureDecay;
 
     vector <vector<int>> graphTree;
-    vector <int> connections;
 
-    for(int i = 0; i < numberOfCD; i++){
-        
+    cin.ignore(INT64_MAX, '\n');
+    for(int i = 0; i < numberOfCD+numberOfPV; i++){
+        vector <int> connections;
+
+        getline(cin, entry);
+        istringstream iss(entry);
+
+        for(string entry; iss >> entry;){
+            connections.push_back(stoi(entry));
+        }
+
+        graphTree.push_back(connections);
     }
-
-    for(int i = numberOfCD; i < numberOfPV+numberOfCD; i++){
-
-    }
-
 
     return 0;
 }
