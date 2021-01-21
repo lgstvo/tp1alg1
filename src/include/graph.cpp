d2e1ee4800b2793cb7120bd89910f388c2858c00
lgstvo <lgustavo2001@gmail.com>
2021-01-21 17:20:16 -0300

    DFS walking completion

--- src/include/graph.cpp
+++ src/include/graph.cpp
@@ -2,6 +2,7 @@
 #include <stack>
 #include <cstdlib>
 #include <iostream>
+#include <queue>
 
 GraphTree::GraphTree(std::vector<std::vector<int>> adjencyList){
     this->adjencyList = adjencyList;
@@ -38,10 +39,11 @@ void GraphTree::setTemperatureDecay(int temperatureDecay){
 void GraphTree::setAdjencyList(std::vector<std::vector<int>> adjencyList){
     this->adjencyList = adjencyList;
 }
-
+/*
 std::vector<int> GraphTree::dfs(std::vector<bool> explored){
     // Valores de indice dos PVs são os indices originais + nCD
     std::vector<int> reachedPV;
+    int deploymentRange = 30/getTemperatureDecay();
 
     for(int i = 0; i < getCD(); i++){
         std::stack<int> notVisited;
@@ -63,8 +65,52 @@ std::vector<int> GraphTree::dfs(std::vector<bool> explored){
                 }
             }
         }
+
+        
     }
-    
+
+    return reachedPV;
+}
+*/
+
+std::vector<int> GraphTree::dfs(std::vector<bool> explored){
+    // Valores de indice dos PVs são os indices originais + nCD
+    std::vector<int> reachedPV;
+    int deploymentRange = 30/getTemperatureDecay();
+
+    for(int i = 0; i < getCD(); i++){
+        std::vector<std::vector<int>> adjencyList = getAdjencyList();
+        std::queue<int> primaryQueue;
+        std::queue<int> decendentQueue;
+        
+        int currentExploration;
+        for(auto it = adjencyList[i].begin(); it != adjencyList[i].end(); it++){
+            primaryQueue.push(*it);
+        }
+
+        for(deploymentRange; deploymentRange > 0; deploymentRange--){
+            while(!(primaryQueue.empty())){
+                int element = primaryQueue.front();
+                if(element != 0){
+                    reachedPV.push_back(element);
+                    int elementIndex = element -1 + getCD();
+                    primaryQueue.pop();
+                    
+                    if(!(explored[elementIndex])){
+                        explored[elementIndex] = 1;
+                        std::vector<int> edges = adjencyList[elementIndex];
+                        for(auto it = edges.begin(); it != edges.end(); it++){
+                            decendentQueue.push(*it);
+                        }
+                    }
+                }
+
+            }
+            // swap queues
+            primaryQueue.swap(decendentQueue);
+        }
+
+    }
 
     return reachedPV;
 }
